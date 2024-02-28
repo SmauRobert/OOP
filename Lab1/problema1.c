de3dc67e401ddcfd83e20617a010d1afe902d1ab
SmauRobert <robertgeorgesmau@gmail.com>
2024-02-28 19:05:50 +0200

    primul lab

--- Lab1/problema1.c
+++ Lab1/problema1.c
@@ -10,14 +10,23 @@ int main(int argc, char **argv) {
 
     char text[100];
     int sum = 0;
+    int lineIndex = 1;
     while(fgets(text, 100, file)) {
-        int curr = 0;
+        int curr = 0, sign = 1;
         for(int i = 0; text[i] != '\0'; i ++)
             if(text[i] >= '0' && text[i] <= '9') curr = curr * 10 + text[i] -'0';
-            else {
-                sum += curr;
+            else if(text[i] == '-') {
+                if(curr != 0) {
+                    printf("Number index %d is invalid\n", lineIndex);
+                    return -1;
+                }
+                sign = -1;
+            } else {
+                sum += curr * sign;
                 curr = 0;
+                sign = 1;
             }
+        lineIndex ++;
     }
 
     printf("%d\n", sum);
