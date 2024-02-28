d8b133726fc1473e307b7773efd43de02513be6b
SmauRobert <robertgeorgesmau@gmail.com>
2024-02-28 19:32:01 +0200

    primul lab

--- Lab1/problema2.c
+++ Lab1/problema2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main() {
+    char sorted[100][100];
+    int wordCount = 0;
+    scanf("%c");
+    while(scanf("%s ", sorted[wordCount])) {
+        if(sorted[wordCount][0] == '\"') break;
+
+        printf("%s\n", sorted[wordCount]);
+        wordCount ++;
+    }
+
+    for(int i = 0; i < wordCount; i ++)
+        printf("%s\n", sorted[i]);
+
     return 0;
 }
